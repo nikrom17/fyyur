7d8dfbc15ac82e500d034a0fd51bd3f9bc557b3a
Nik <nikrom17@gmail.com>
2020-05-11 18:46:17 -0500

    add migrations to app

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import json
 import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
@@ -17,6 +18,7 @@ from forms import *
 #----------------------------------------------------------------------------#
 
 app = Flask(__name__)
+migrate = Migrate(app, db)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
