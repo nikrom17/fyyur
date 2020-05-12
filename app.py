e56da26cc350ce43539326aa735bb83212eb2c11
Nik <nikrom17@gmail.com>
2020-05-12 12:40:25 -0500

    fetch and view single artist

--- app.py
+++ app.py
@@ -445,8 +445,13 @@ def show_artist(artist_id):
     "past_shows_count": 0,
     "upcoming_shows_count": 3,
   }
-  data = list(filter(lambda d: d['id'] == artist_id, [data1, data2, data3]))[0]
-  return render_template('pages/show_artist.html', artist=data)
+  # data = list(filter(lambda d: d['id'] == artist_id, [data1, data2, data3]))[0]
+  data = Artist.query.get(artist_id)
+  print(data)
+  if data:
+    return render_template('pages/show_artist.html', artist=data)
+  else:
+    return render_template('errors/404.html')
 
 #  Update
 #  ----------------------------------------------------------------
