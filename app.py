505fc68a9642810b57a4703e7db3686b86fae1ec
Nik <nikrom17@gmail.com>
2020-05-12 08:03:15 -0500

    add genres enum

--- app.py
+++ app.py
@@ -77,6 +77,27 @@ class Show(db.Model):
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
+
+class Genres(Enum):
+  Alternative = 'Alternative',
+  Blues = 'Blues',
+  Classical = 'Classical',
+  Country = 'Country',
+  Electronic = 'Electronic',
+  Folk = 'Folk',
+  Funk = 'Funk',
+  Hip_Hop = 'Hip-Hop',
+  Heavy_Metal = 'Heavy Metal',
+  Instrumental = 'Instrumental',
+  Jazz = 'Jazz',
+  Musical_Theatre = 'Musical Theatre',
+  Pop = 'Pop',
+  Punk = 'Punk',
+  R_n_B = 'R&B',
+  Reggae = 'Reggae',
+  Rock_n_Roll = 'Rock n Roll',
+  Soul = 'Soul',
+  Other = 'Other',
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
